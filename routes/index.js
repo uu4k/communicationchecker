d5d1d886cd11d599f4317c233cc8db0a0924d295
uu4k <t.shimizu1115@gmail.com>
2019-11-04 08:09:22 +0900

    implement communication checker.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  const check_url = req.query["url"];
+  if (check_url) {
+    axios.get(check_url).then((result) => {
+      res.render("index", { title: "Communication Checker", url: check_url, result: result });
+    });
+  } else {
+    res.render("index", { title: "Communication Checker" });
+  }
 });
 
 module.exports = router;
